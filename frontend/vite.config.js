b600d5ea785f66f39e4b51866b78010ac383bbaa
Kai_Office <kai.huang2@medtronic.com>
2026-07-18 12:21:30 +0800

    Support direct stock port access

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/stock/",
+  base: "./",
   plugins: [react()],
   server: {
     allowedHosts: ["czops.site", "www.czops.site", "175.24.139.233"],
